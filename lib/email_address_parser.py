c0363ae3b68d6d64bf53424e475ffcfcf424df3a
Saki <sbasken@gmail.com>
2023-03-16 21:41:59 -0700

    create class EmailAddressParser class with parse method to find emails in a string

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,14 @@
 # your code goes here!
+import re
+
+class EmailAddressParser():
+
+    def __init__(self, string):
+        self.string = string
+
+    def parse(self):
+        pattern = r"[\w\.\w]+@[\w]+.com"
+        regex = re.compile(pattern)
+        match = regex.findall(self.string)
+        match.reverse()
+        return match
